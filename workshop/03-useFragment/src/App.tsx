22b20bfdf11b0054cb6215ee17b9c783d802dc0e
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-04-18 10:07:36 -0300

    feat(workshop): 03-useFragment exercise

---
+++ workshop/03-useFragment/src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { Flex, Text } from 'rebass';
+import { Content } from '@workshop/ui';
+import { useLazyLoadQuery, graphql } from 'react-relay/hooks';
+
+import Post from './Post';
+
+import { AppQuery } from './__generated__/AppQuery.graphql';
+
+const App = () => {
+  const response = useLazyLoadQuery<AppQuery>(
+    graphql`
+      query AppQuery {
+        posts(first: 10) {
+          edges {
+            node {
+              id
+              ...Post_post
+            }
+          }
+        }
+      }
+    `,
+    {},
+    {
+      fetchPolicy: 'network-only',
+    },
+  );
+
+  const { posts } = response;
+
+  return (
+    <Content>
+      <Flex flexDirection='column'>
+        <Text>Posts</Text>
+        <Flex flexDirection='column'>
+          {posts.edges.map(({ node }) => (
+            <Post key={node.id} post={node} />
+          ))}
+        </Flex>
+      </Flex>
+    </Content>
+  );
+};
+
+export default App;
---
+++ workshop/03-useFragment/src/index.tsx
@@ -0,0 +1,7 @@
+import 'isomorphic-fetch';
+import React from 'react';
+import { createRoot } from 'react-dom';
+
+import Root from './Root';
+
+createRoot(document.getElementById('root')).render(<Root />);
