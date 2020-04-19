ce2a1c0343ece323c3603b18b31913514698aeb4
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2020-04-19 08:35:10 -0300

    feat(workshop): 07-useRefetchableFragment

---
+++ workshop/07-useRefetchableFragment/README.md
@@ -0,0 +1,11 @@
+# 07 - useRefetchableFragment
+
+Learn how to use refetch any query using useRefetchableFragment
+
+## Exercise
+
+- modify PostComments to use useRefetchableFragment and a button to refetch old comments
+
+## Extras
+
+- [ ] add a button to refetch new comments
---
+++ workshop/07-useRefetchableFragment/src/comment/PostCommentCreateMutation.tsx
@@ -0,0 +1,64 @@
+import { graphql } from 'react-relay';
+import { SelectorStoreUpdater, RecordSourceSelectorProxy, ConnectionHandler } from 'relay-runtime';
+
+import { connectionUpdater } from '@workshop/relay';
+
+import { PostCommentCreateInput } from './__generated__/PostCommentCreateMutation.graphql';
+import { PostCommentComposer_me } from './__generated__/PostCommentComposer_me.graphql';
+
+export const PostCommentCreate = graphql`
+  mutation PostCommentCreateMutation($input: PostCommentCreateInput!) {
+    PostCommentCreate(input: $input) {
+      success
+      error
+      post {
+        commentsCount
+      }
+      commentEdge {
+        node {
+          id
+          body
+          user {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const updater = (parentId: string): SelectorStoreUpdater => (store: RecordSourceSelectorProxy) => {
+  const newEdge = store.getRootField('PostCommentCreate').getLinkedRecord('commentEdge');
+
+  connectionUpdater({
+    store,
+    parentId,
+    connectionName: 'PostComments_comments',
+    edge: newEdge,
+    before: true,
+  });
+};
+
+let tempID = 0;
+
+export const optimisticUpdater = (input: PostCommentCreateInput, me: PostCommentComposer_me) => (
+  store: RecordSourceSelectorProxy,
+) => {
+  const id = 'client:newComment:' + tempID++;
+
+  const node = store.create(id, 'Comment');
+
+  const meProxy = store.get(me.id);
+
+  node.setValue(id, 'id');
+  node.setValue(input.body, 'body');
+  node.setLinkedRecord(meProxy, 'user');
+
+  const newEdge = store.create('client:newEdge:' + tempID++, 'CommentEdge');
+  newEdge.setLinkedRecord(node, 'node');
+
+  const parentProxy = store.get(input.post);
+  const conn = ConnectionHandler.getConnection(parentProxy, 'PostComments_comments');
+  ConnectionHandler.insertEdgeBefore(conn, newEdge);
+};
