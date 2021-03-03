eae0171c1c310375c47c7afab585c564ef8670d5
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2021-03-02 23:57:37 -0300

    update realy

--- packages/relay/src/index.tsx
+++ packages/relay/src/index.tsx
@@ -1,6 +1,3 @@
 export { connectionUpdater, connectionDeleteEdgeUpdater } from './mutationUtils';
 
 // TODO - consume these 3 functions directly from react-relay
-export { useSubscription } from './useSubscription';
-export { loadQuery } from './loadQuery';
-export { useMutation } from './useMutation';
