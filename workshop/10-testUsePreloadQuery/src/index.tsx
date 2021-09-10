43c8ece51c9718854e5e13b8843ab47abbceda76
Tsugami <yslanramos.r@gmail.com>
2021-09-10 14:17:58 -0300

    feat(workshop): convert new react 18v createRoot

--- workshop/10-testUsePreloadQuery/src/index.tsx
+++ workshop/10-testUsePreloadQuery/src/index.tsx
@@ -1,7 +1,13 @@
-import 'isomorphic-fetch';
 import React from 'react';
+import 'isomorphic-fetch';
 import ReactDOM from 'react-dom';
 
 import Root from './Root';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const container = document.getElementById('root');
+
+if (!container) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<Root />);
