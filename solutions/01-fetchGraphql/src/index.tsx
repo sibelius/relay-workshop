4300e427991e4d742c1a4867b83ef9c75b5fb5c3
Tsugami <yslanramos.r@gmail.com>
2021-09-10 14:20:09 -0300

    feat(solutions): convert new react 18v createRoot

--- solutions/01-fetchGraphql/src/index.tsx
+++ solutions/01-fetchGraphql/src/index.tsx
@@ -1,7 +1,13 @@
-import 'isomorphic-fetch';
 import React from 'react';
+import 'isomorphic-fetch';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+
+if (!container) throw new Error('Failed to find the root element');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
