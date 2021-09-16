332b16f1ca7a31a46d3115683d5b18d34760203a
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2021-09-16 10:32:47 -0300

    feat(wp): wp prod

---
+++ packages/webpack/src/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotEnv = require('dotenv-webpack');
+const webpack = require('webpack')
+
+const cwd = process.cwd();
+const outputPath = path.join(cwd, 'build');
+
+module.exports = {
+  context: path.resolve(cwd, './'),
+  entry: ['./src/index.tsx'],
+  output: {
+    path: outputPath,
+    publicPath: '/',
+    pathinfo: false,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.mjs'],
+    fallback: {
+      fs: false,
+      net: false,
+      path: require.resolve('path-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "assert": require.resolve("assert/")
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)?$/,
+        exclude: [/node_modules/],
+        use: ['babel-loader?cacheDirectory'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|pdf|csv|xlsx|ttf|woff(2)?)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new dotEnv({
+      path: './.env',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    })
+  ],
+};
