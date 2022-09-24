c0a11e6051b78453ee62169ebccda047b5ce15ba
diegochueri <diegochueri@gmail.com>
2022-09-24 17:56:45 -0400

    feat(graphql-ws): add graphql-ws instead of subscriptions-transport-ws on server

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,46 +1,19 @@
+import { useServer } from 'graphql-ws/lib/use/ws';
 import { createServer } from 'http';
-
-import { SubscriptionServer } from 'subscriptions-transport-ws';
-
-import { execute, subscribe } from 'graphql';
-
+import ws from 'ws';
 import app from './app';
-import { connectDatabase } from './database';
 import { config } from './config';
-
+import { connectDatabase } from './database';
 import { schema } from './schema/schema';
-import { getContext } from './getContext';
-import { getUser } from './auth';
 
-type ConnectionParams = {
-  authorization?: string;
-};
 (async () => {
   await connectDatabase();
 
   const server = createServer(app.callback());
 
   server.listen(config.PORT, () => {
-    // eslint-disable-next-line
     console.log(`server running on port :${config.PORT}`);
   });
-
-  SubscriptionServer.create(
-    {
-      onConnect: async (connectionParams: ConnectionParams) => {
-        const { user } = await getUser(connectionParams.authorization);
-
-        return await getContext({ user });
-      },
-      // eslint-disable-next-line
-      onDisconnect: () => console.log('Client subscription disconnected!'),
-      execute,
-      subscribe,
-      schema,
-    },
-    {
-      server,
-      path: '/subscriptions',
-    },
-  );
+  const graphqlWs = new ws.Server({ server });
+  useServer({ schema }, graphqlWs);
 })();
