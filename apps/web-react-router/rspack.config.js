892de82aefdb1dc7d98bbb470fd251573d98c510
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2023-11-15 09:33:17 -0300

    feat(rspack): rspack

---
+++ apps/web-react-router/rspack.config.js
@@ -0,0 +1,69 @@
+const rspack = require('@rspack/core');
+const NodePolyfill = require('@rspack/plugin-node-polyfill');
+
+const cwd = process.cwd();
+
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  context: cwd,
+  entry: {
+    main: './src/index.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)?$/,
+        exclude: [/node_modules(?!\/@woovi)/],
+        loader: 'builtin:swc-loader',
+        options: {
+          jsc: {
+            experimental: {
+              plugins: [
+                [
+                  '@swc/plugin-relay',
+                  {
+                    rootDir: cwd,
+                    language: 'typescript',
+                  },
+                ],
+              ],
+            },
+            parser: {
+              syntax: 'typescript',
+              tsx: true,
+            },
+            transform: {
+              react: {
+                runtime: 'automatic',
+              },
+            },
+          },
+        },
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|mp3|pdf|csv|xlsx|ttf|woff(2)?)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [
+    new NodePolyfill(),
+    new rspack.HtmlRspackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  builtins: {
+    relay: true,
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+    },
+  },
+};
