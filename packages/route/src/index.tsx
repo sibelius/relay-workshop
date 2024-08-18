7ec5893424aeacc2f8411d4f14d46c2ec802428b
HallexCosta <hallex.costa@hotmail.com>
2024-08-17 23:25:34 -0300

    feat: remove package route

--- packages/route/src/index.tsx
+++
@@ -1,8 +0,0 @@
-export { default as RoutingContext, useRoutingContext } from './RoutingContext';
-export { default as createRouter } from './createRouter';
-export { getMockHistory } from './getMockHistory';
-export { default as RouterRenderer } from './RouterRenderer';
-export { default as JSResource } from './JSResource';
-export { useHistory } from './useHistory';
-export { default as Link } from './Link';
-export { usePreloadRoute } from './usePreloadRoute';
