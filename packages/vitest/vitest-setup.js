a28c43f4324e4384675f9361d032ab2313244117
HallexCosta <hallex.costa@hotmail.com>
2024-08-27 22:22:18 -0300

    feat: import user-event from testing-library

--- packages/vitest/vitest-setup.js
+++ packages/vitest/vitest-setup.js
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom/vitest'
+import '@testing-library/user-event'
 // eslint-disable-next-line import/namespace
 import { vi } from 'vitest'
 global.IS_REACT_ACT_ENVIRONMENT = true
+// replace jest api to vitest api
 global.jest = vi
