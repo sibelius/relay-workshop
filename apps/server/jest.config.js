a0044df9cfa5037e4d30c4d8f1f5a98ed16eaed4
HallexCosta <hallex.costa@hotmail.com>
2024-09-20 13:00:31 -0300

    style: apply lint

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -1,34 +1,34 @@
-const pack = require('./package.json');
+const pack = require("./package.json");
 
 const jestTransformer = () => {
-  if (process.env.JEST_TRANSFORMER === 'babel-barrel') {
-    // eslint-disable-next-line
-    console.log('babel-barrel');
+	if (process.env.JEST_TRANSFORMER === "babel-barrel") {
+		// eslint-disable-next-line
+		console.log("babel-barrel");
 
-    return {
-      '^.+\\.(js|ts|tsx)?$': require.resolve('./babelBarrel'),
-    }
-  }
+		return {
+			"^.+\\.(js|ts|tsx)?$": require.resolve("./babelBarrel"),
+		};
+	}
 
-  // eslint-disable-next-line
-  console.log('babel-jest');
+	// eslint-disable-next-line
+	console.log("babel-jest");
 
-  return {
-    '^.+\\.(js|ts|tsx)?$': '<rootDir>/test/babel-transformer',
-  };
+	return {
+		"^.+\\.(js|ts|tsx)?$": "<rootDir>/test/babel-transformer",
+	};
 };
 
 module.exports = {
-  displayName: pack.name,
-  testEnvironment: '<rootDir>/test/environment/mongodb',
-  testPathIgnorePatterns: ['/node_modules/', './dist'],
-  coverageReporters: ['lcov', 'html'],
-  setupFilesAfterEnv: ['<rootDir>/test/setupTestFramework.js'],
-  resetModules: false,
-  reporters: ['default', 'jest-junit'],
-  transform: {
-    ...jestTransformer(),
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts|tsx)?$',
-  moduleFileExtensions: ['ts', 'js', 'tsx', 'json'],
+	displayName: pack.name,
+	// testEnvironment: '<rootDir>/test/environment/mongodb',
+	testPathIgnorePatterns: ["/node_modules/", "./dist"],
+	coverageReporters: ["lcov", "html"],
+	setupFilesAfterEnv: ["<rootDir>/test/setupTestFramework.js"],
+	resetModules: false,
+	reporters: ["default", "jest-junit"],
+	transform: {
+		...jestTransformer(),
+	},
+	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts|tsx)?$",
+	moduleFileExtensions: ["ts", "js", "tsx", "json"],
 };
