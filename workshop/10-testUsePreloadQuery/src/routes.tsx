a443218782d237e57a38586f15a30e46ac038fa0
HallexCosta <hallex.costa@hotmail.com>
2024-09-20 13:01:38 -0300

    style: apply lint

--- workshop/10-testUsePreloadQuery/src/routes.tsx
+++ workshop/10-testUsePreloadQuery/src/routes.tsx
@@ -1,53 +1,49 @@
-import React from 'react'
-import { loadQuery } from 'react-relay';
+import React from "react";
+import { loadQuery } from "react-relay";
 
-import { RouteObject } from 'react-router-dom';
+import type { RouteObject } from "react-router-dom";
 
-import { Environment } from './relay';
+import { Environment } from "./relay";
 
+import AppQuery from "./__generated__/AppQuery.graphql";
+import PostDetailQuery from "./components/feed/post/__generated__/PostDetailQuery.graphql";
 
-import AppQuery from './__generated__/AppQuery.graphql'
-import PostDetailQuery from './components/feed/post/__generated__/PostDetailQuery.graphql'
-
-import App from './App';
-import PostDetail from './components/feed/post/PostDetail';
-
+import App from "./App";
+import PostDetail from "./components/feed/post/PostDetail";
 
 export const routes: RouteObject[] = [
-  {
-    element: <App />,
-    path: '/',
-    loader: () => {
-
-      return {
-        appQuery: loadQuery(
-          Environment,
-          AppQuery,
-          {},
-          {
-            fetchPolicy: 'network-only',
-          },
-        ),
-      };
-    },
-  },
-  {
-    path: '/post/:id',
-    element: <PostDetail />,
-    loader: ({ params }) => {
-
-      return {
-        postDetailQuery: loadQuery(
-          Environment,
-          PostDetailQuery,
-          {
-            id: params.id,
-          },
-          {
-            fetchPolicy: 'store-or-network',
-          },
-        ),
-      };
-    },
-  },
+	{
+		element: <App />,
+		path: "/",
+		loader: () => {
+			return {
+				appQuery: loadQuery(
+					Environment,
+					AppQuery,
+					{},
+					{
+						fetchPolicy: "network-only",
+					},
+				),
+			};
+		},
+	},
+	{
+		path: "/post/:id",
+		element: <PostDetail />,
+		loader: ({ params }) => {
+			return {
+				postDetailQuery: loadQuery(
+					Environment,
+					PostDetailQuery,
+					{
+						id: params.id,
+					},
+					{
+						fetchPolicy: "store-or-network",
+					},
+				),
+			};
+		},
+	},
 ];
