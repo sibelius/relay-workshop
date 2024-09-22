0722bb163223807ed56f01c683935bdd43c81767
HallexCosta <hallex.costa@hotmail.com>
2024-09-21 21:42:43 -0300

    chore: adjust some test in workshop workspace

--- solutions/09-usePreloadedQuery/src/routes.tsx
+++ solutions/09-usePreloadedQuery/src/routes.tsx
@@ -1,35 +1,50 @@
+import React from "react";
+import { loadQuery } from "react-relay";
 
-import React from 'react';
-import { loadQuery } from 'react-relay';
+import type { RouteObject } from "react-router-dom";
 
-import { RouteObject } from 'react-router-dom'
+import { AppQuery } from "./__generated__/AppQuery.graphql";
+import { PostDetailQuery } from "./__generated__/AppQuery.graphql";
 
-import App from './App';
-import PostDetail from './components/feed/post/PostDetail';
+import App from "./App";
+import PostDetail from "./components/feed/post/PostDetail";
 
 // eslint-disable-next-line
-import { Environment } from './relay';
+import { Environment } from "./relay";
 
 export const routes: RouteObject[] = [
-  {
-    element: <App />,
-    path: '/',
-    loader: () => {
-      /**
-       * TODO
-       * add preloadQuery to start fetching before component has mounted
-       */
-    },
-  },
-  {
-    path: '/post/:id',
-    element: <PostDetail />,
-    loader: ({ request, params, context }) => {
-      /**
-       * TODO
-       * add preloadQuery to start fetching before component has mounted
-       * use params as query variables
-       */
-    },
-  },
+	{
+		element: <App />,
+		path: "/",
+		loader: () => {
+			return {
+				appQuery: loadQuery(
+					Environment,
+					AppQuery,
+					{},
+					{
+						fetchPolicy: "network-only",
+					},
+				),
+			};
+		},
+	},
+	{
+		path: "/post/:id",
+		element: <PostDetail />,
+		loader: ({ request, params, context }) => {
+			return {
+				postDetailQuery: loadQuery(
+					Environment,
+					PostDetailQuery,
+					{
+						id: params.id,
+					},
+					{
+						fetchPolicy: "store-or-network",
+					},
+				),
+			};
+		},
+	},
 ];
--- workshop/09-usePreloadedQuery/src/routes.tsx
+++ workshop/09-usePreloadedQuery/src/routes.tsx
@@ -3,9 +3,6 @@ import { loadQuery } from "react-relay";
 
 import type { RouteObject } from "react-router-dom";
 
-import { AppQuery } from "./__generated__/AppQuery.graphql";
-import { PostDetailQuery } from "./__generated__/AppQuery.graphql";
-
 import App from "./App";
 import PostDetail from "./components/feed/post/PostDetail";
 
@@ -17,16 +14,6 @@ export const routes: RouteObject[] = [
 		element: <App />,
 		path: "/",
 		loader: () => {
-			return {
-				appQuery: loadQuery(
-					Environment,
-					AppQuery,
-					{},
-					{
-						fetchPolicy: "network-only",
-					},
-				),
-			};
 			/**
 			 * TODO
 			 * add preloadQuery to start fetching before component has mounted
@@ -37,18 +24,11 @@ export const routes: RouteObject[] = [
 		path: "/post/:id",
 		element: <PostDetail />,
 		loader: ({ request, params, context }) => {
-			return {
-				postDetailQuery: loadQuery(
-					Environment,
-					PostDetailQuery,
-					{
-						id: params.id,
-					},
-					{
-						fetchPolicy: "store-or-network",
-					},
-				),
-			};
+			/**
+			 * TODO
+			 * add preloadQuery to start fetching before component has mounted
+			 * use params as query variables
+			 */
 		},
 	},
 ];
